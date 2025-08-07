fa3bef5c58c73f699b5ca82fa7972c7f11e21186
Amory Hervet <160531843+Yromale1@users.noreply.github.com>
2025-08-07 16:18:01 +0200

    Change folder name "data" to "dataset"

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create the target directory if it doesn't exist
-mkdir -p ./data
+mkdir -p ./dataset
 
 # Download Abstract Art Gallery dataset from Kaggle into ./data/
-curl -L -o ./data/abstract-art-gallery.zip https://www.kaggle.com/api/v1/datasets/download/bryanb/abstract-art-gallery
+curl -L -o ./dataset/abstract-art-gallery.zip https://www.kaggle.com/api/v1/datasets/download/bryanb/abstract-art-gallery
